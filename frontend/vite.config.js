fc2f6abb46ce17f82ac10b75b47bd7b96d838b6f
somenath singh <144797336+itsnarutouzumaki@users.noreply.github.com>
2025-06-01 15:14:23 +0530

    netlify configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        // Ensure clean filenames (no hashes)
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+  base: "/", // Critical for Netlify deployments
 });
