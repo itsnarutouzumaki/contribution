68602fbe20985bcd28e144b78e7ee79efc4a2e9e
somenath singh <144797336+itsnarutouzumaki@users.noreply.github.com>
2025-06-02 16:53:23 +0530

    setting as per frontend hosted site

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,24 @@ dotenv.config();
 const app = express();
 const PORT = 4000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://assystantcontribution.netlify.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 
 app.use(express.json());
 app.use(cookieParser());
